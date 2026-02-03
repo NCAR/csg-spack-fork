265d707d2397ff6c4e480adbce2f8f71d8b39eb2
Brian <vanderwb@ucar.edu>
2026-02-02 18:31:51 -0700

    Add debug env support

--- var/spack/repos/spack_repo/ncar.hpcd/packages/ncarcompilers/package.py
+++ var/spack/repos/spack_repo/ncar.hpcd/packages/ncarcompilers/package.py
@@ -21,6 +21,7 @@ class Ncarcompilers(MakefilePackage):
     maintainers = ['vanderwb']
 
     version('main',  branch='main')
+    version('1.2.0', sha256='6b371891a39642115b38b0a05c688575e9c5c56f5393eff1c3d200cfaa74d4aa')
     version('1.1.0', sha256='ebfb6b635c1270e318d3cda688a2f2971cd5ac1aed963a7556fe9214c08201bb')
     version('1.0.0', sha256='99e08722cbfd7aeeb7dfaec18ae9753f9beeeeddfd37b9dc1b0ea7ed40b249d9')
     version('0.8.0', sha256='f9049ee4d63f52c3c971604b72e41d27a913e87b968b5c7f2cbcb08871affed9')
